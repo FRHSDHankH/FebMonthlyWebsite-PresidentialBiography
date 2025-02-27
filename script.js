39d6f8449c90d96c077742d8b5eea527694e63e3
Hank <155669403+FRHSDHankH@users.noreply.github.com>
2025-02-27 23:46:19 +0000

    Timeline done, and time is ticking 😭😭😭

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ let quotes = [
 let randomNum = Math.floor(Math.random() * quotes.length)
 
 quote.innerText = quotes[randomNum]
+
+function march() {
+  document.getElementById('titleChange').innerText = 'Inauguration as 31st President'
+  document.getElementById('imgChange').src = 'imgs/HooverInaug.jpg'
+  document.getElementById('descriptionChange').innerText = 'Herbert Hoover was inaugurated as the 31st President of the United States, promising continued prosperity, but just months later, the stock market crashed, leading to the Great Depression.'
+}
+
+function october() {
+  document.getElementById('titleChange').innerText = 'Stock Market Crash ("Black Tuesday")'
+  document.getElementById('imgChange').src = 'imgs/HooverStock.jpg'
+  document.getElementById('descriptionChange').innerText = 'The stock market collapsed, marking the beginning of the Great Depression. Despite Hoover\'s efforts to stabilize the economy, the crisis deepened, and his response was widely criticized.'
+}
+
+function june() {
+  document.getElementById('titleChange').innerText = 'Signing of the Smoot-Hawley Tariff Act'
+  document.getElementById('imgChange').src = 'imgs/HooverTariff.jpg'
+  document.getElementById('descriptionChange').innerText = 'Hoover signed the Smoot-Hawley Tariff, which raised import duties in an attempt to protect American businesses but worsened the global economic downturn by reducing international trade.'
+}
+
+function july() {
+  document.getElementById('titleChange').innerText = 'Bonus Army Confrontation'
+  document.getElementById('imgChange').src = 'imgs/HooverBonus.jpg'
+  document.getElementById('descriptionChange').innerText = 'Thousands of World War I veterans, known as the Bonus Army, marched on Washington, D.C., demanding early payment of their promised bonuses. Hoover ordered their removal, leading to a violent clash that further damaged his reputation.'
+}
